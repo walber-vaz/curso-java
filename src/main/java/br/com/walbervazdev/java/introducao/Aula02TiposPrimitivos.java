e39a40b9fc9a250f128b3a677f85e835a3571045
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-08-12 08:48:13 -0300

    feat: types primitive

---
+++ src/main/java/br/com/walbervazdev/java/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,25 @@
+package br.com.walbervazdev.java.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        System.out.println("Tipos primitivos");
+        // int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        double salaryDouble = 2000;
+        float salaryFloat = 2500;
+        byte ageByte = 10;
+        short ageShort = 10;
+        long bigNumber = 100000L;
+        char character = 'A';
+        boolean isTrue = true;
+
+        System.out.println("int: " + age);
+        System.out.println("double: " + salaryDouble);
+        System.out.println("float: " + salaryFloat);
+        System.out.println("byte: " + ageByte);
+        System.out.println("short: " + ageShort);
+        System.out.println("long: " + bigNumber);
+        System.out.println("char: " + character);
+        System.out.println("boolean: " + isTrue);
+    }
+}
